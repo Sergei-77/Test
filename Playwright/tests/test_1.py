87b8a80bb5732342ff6e4910875bbeb3cfffe7c5
ADMIN <seregabullu89@gmail.com>
2022-12-16 13:47:04 +0300

    changes_test_name

--- Playwright/tests/test_1.py
+++ Playwright/tests/test_1.py
@@ -7,7 +7,7 @@ def test_request_status_code():
     r = requests.get(url=url, params=params)
     assert r.status_code == 200
     
-def test_request_ya():
+def test_request_yandex():
     url = 'https://ya.ru'
     r = requests.get(url=url)
     assert r.status_code == 200
